46261358788d503d93cdea39b28d7f703e967b69
Sabrina Tolle <sabrinatolle@outlook.com>
2018-08-27 10:49:32 -0400

    add player movement

--- js/app.js
+++ js/app.js
@@ -6,12 +6,15 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
+
+
+
+
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
@@ -27,22 +30,69 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 class Hero {
     constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.sprite = 'images/char-boy.png'
+        this.sprite = 'images/char-boy.png';
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY =( this.jump * 5) - 20; // padding to center hero
+        this.x = this.startX;
+        this.y = this.startY;
+        
+        
     }
+
+    render() 
+{
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+
 
 // This class requires an update(), render() and
 // a handleInput() method.
-render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+/**
+ * @param {string} input - Direction to travel 
+ */
+
+
+handleInput(input)
+ {
+switch(input) {
+    case 'left':
+    if (this.x > 0) {
+        this.x -= this.step;
+
+    } 
+        break;
+    case 'up':
+        if (this.y > this.jump) {
+            this.y -= this.jump;
+        } 
+        break;
+    case 'right':
+        if (this.x < this.step *4) {
+            this.x += this.step;
+        }
+        break;
+    case 'down':
+        if (this.y < this.jump* 4) {
+            this.y += this.jump;
+        }
+        break;
+    }
+
 }
-}
-const player = new Hero();
+};
+
+
+
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+const player = new Hero();
 
 
 // This listens for key presses and sends the keys to your
